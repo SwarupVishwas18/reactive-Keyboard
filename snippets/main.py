eb37b5550d5d916bf4c5a0d756e15f5931545a1a
Swarup Vishwas <swarupvishwas000@gmail.com>
2024-04-13 13:08:08 +0530

    Integration for one container

---
+++ snippets/main.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt6 import QtWidgets, uic
+from frontend.main_window import MainWindow
+from ex import get_data
+
+data = get_data("donut")
+
+app = QtWidgets.QApplication(sys.argv)
+
+window = MainWindow(data=data)
+window.show()
+app.exec()
